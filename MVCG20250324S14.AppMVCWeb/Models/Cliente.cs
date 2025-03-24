1753a8f8e5340b9d466c4f70b8dec4a8daeb28fd
Marcos Gutiérrez <marcosgtzx7@hotmail.com>
2025-03-24 15:32:34 -0600

    Se les dió diseño a las vistas, create, delete, detail, edits y index

--- MVCG20250324S14.AppMVCWeb/Models/Cliente.cs
+++ MVCG20250324S14.AppMVCWeb/Models/Cliente.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace MVCG20250324S14.AppMVCWeb.Models;
 
 public partial class Cliente
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string Nombre { get; set; } = null!;
 
+    [Display(Name = "Dirección")]
     public string? Direccion { get; set; }
 
+
+    [Display(Name = "Teléfono")]
     public string? Telefono { get; set; }
 
     public string? Email { get; set; }
